ed059db451e5fc93fa2568296b1473fec1816edf
holger.geisler <holger.geisler@alpenblick.gmbh>
2025-11-24 10:35:32 +0000

    Update repository

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,13 +8,12 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyDSlB5z-NparOYnEPGWjY2KQ9MEEPuxDJY",
+  authDomain: "ab-rename-46741219-ba4bc.firebaseapp.com",
+  projectId: "ab-rename-46741219-ba4bc",
+  storageBucket: "ab-rename-46741219-ba4bc.firebasestorage.app",
+  messagingSenderId: "600072389128",
+  appId: "1:600072389128:web:648657cf4c2fbe2d94c7bd"
 };
 
 // Initialize Firebase
